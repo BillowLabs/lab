71cd8eaccacafec0449e96704bc9952bcc85c504
Michael Huensch <setup@run00.com>
2017-05-02 23:18:36 -0500

    Added adapter methods to handle account update, delete, and read

---
+++ build/build.sh
@@ -0,0 +1,3 @@
+export UNION_USERNAME=mhuensch@billowlabs.com
+export UNION_PASSWORD=dsmh20002Fchyms2r1Gg0faZrYwwvfoe
+ant -lib ./build/libs  -f ./build/build.xml $1
---
+++ resource-bundles/OrgAssets.resource/README.md
@@ -0,0 +1,1 @@
+#Ember Lab Testing
