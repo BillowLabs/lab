074faa025e2687f1666be14fe2a9de3fb4ef37be
Michael Huensch <setup@run00.com>
2017-05-03 17:25:16 -0500

    Refactored store to make code a little more elegant

--- ember/app/services/store.js
+++ ember/app/services/store.js
@@ -5,11 +5,12 @@ export default DS.Store.extend({
 
 	init() {
 		if (window.parent.connect_SFDC) this.hostedOnSalesforce = true
+		this.LocalAdapter = new DS.RESTAdapter()
 		return this._super.apply(this, arguments)
 	},
 
 	adapterFor(modelName) {
-		if (!this.hostedOnSalesforce) return new DS.RESTAdapter()
+		if (!this.hostedOnSalesforce) return this.LocalAdapter
 		return this._super(modelName)
 	}
 
